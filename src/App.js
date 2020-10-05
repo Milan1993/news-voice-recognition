aa2fbddd98704bf8f877819c62f0adcdce6463f6
Milan Velickovic <milan.z.velickovic993@hotmail.com>
2020-10-05 19:05:45 +0200

    Implemented info card on start screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import NewsCards from './components/NewsCards/NewsCards';
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles";
 
 const alanKey =
   "4eeb306e56c79cf75fee990ca7454b672e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const classes = useStyles();
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
@@ -21,7 +24,14 @@ const App = () => {
 
   return (
     <div>
-      <NewsCards articles={articles}/>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          className={classes.alanLogo}
+          alt="logo"
+        />
+      </div>
+      <NewsCards articles={articles} />
     </div>
   );
 };
